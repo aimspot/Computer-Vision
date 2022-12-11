53ad61a80be336c752c1d220f5bdda37c02d560a
aimspot <82668230+aimspot@users.noreply.github.com>
2022-12-11 21:15:19 +0300

    up v 2.0

--- Lab_4/main.py
+++ Lab_4/main.py
@@ -4,14 +4,15 @@ import cvzone
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 from time import sleep
+from pynput import keyboard
 from pynput.keyboard import Controller
 
 class Key():
-    def __init__(self, position, text):
+    def __init__(self, position, text, w = 90, h = 90):
         self.x, self.y = position
         self.text = text
-        self.w = 90
-        self.h = 90
+        self.w = w
+        self.h = h
         self.color = (0, 0, 255)
 
     def draw(self, frame):
@@ -27,19 +28,25 @@ def createKeybord(position, up = False):
     letter_up = [["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "+"],
                 ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]"],
                 ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'"],
-                ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
+                ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"],
+                [" "]]
 
     letter_low = [["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "+"],
                 ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "{", "}"],
                 ["a", "s", "d", "f", "g", "h", "j", "k", "l", ":", '"'],
-                ["z", "x", "c", "v", "b", "n", "m", "<", ">", "?"]]
+                ["z", "x", "c", "v", "b", "n", "m", "<", ">", "?"],
+                [" "]]
     buttons = []
     for i in range(len(letter_up)):
         for j, key in enumerate(letter_up[i]):
-            if up:
+            if up and letter_up[i][j] != " ":
                 buttons.append(Key((x, y), letter_up[i][j]))
             else:
-                buttons.append(Key((x, y), letter_low[i][j]))
+                if letter_up[i][j] == " " or letter_low[i][j] == " ":
+                    buttons.append(Key((x, y), letter_low[i][j], 300, 90))
+                else:
+                    buttons.append(Key((x, y), letter_low[i][j]))
+
             x += 95
         y += 95
         x = old_x
@@ -53,7 +60,7 @@ def drawKeyboard(frame, keybord):
         i.draw(img_new)
     out = frame.copy()
     mask = img_new.astype(bool)
-    out[mask] = cv2.addWeighted(frame, 0.5, img_new, 1 - 0.5, 0)[mask]
+    out[mask] = cv2.addWeighted(frame, 0.7, img_new, 1 - 0.5, 0)[mask]
     return out
 
 def drawButtonSelected(x, y, w, h, text, frame, color):
@@ -72,7 +79,7 @@ def start():
     cap.set(4, 720)
     text = ""
 
-    detector = HandDetector(detectionCon=0.5, maxHands=2)
+    detector = HandDetector(detectionCon=0.5)
     flag = False
     buttons = createKeybord((20, 20), flag)
     keybordCon = Controller()
@@ -93,16 +100,17 @@ def start():
                 w, h = button.w, button.h
 
                 if x < hands[8][0] < x + w and y< hands[8][1] <y + h: # 8 указательный палец, 0 x координата
-                    drawButtonSelected(x, y, w, h, button.text, frame, (0, 255, 255))
-
-
+                    drawButtonSelected(x, y, w, h, button.text, frame, (0, 0, 255))
                     # если дистанция между двумя пальцами 8 и 12 очень маленькая < 60
+
                     l, _, _ = detector.findDistance(8, 12, frame)
                     change_l, _, _ = detector.findDistance(4, 20, frame)
-                    print(l)
+                    delete_l, _, _ = detector.findDistance(4, 13, frame)
+                    enter_l, _, _ = detector.findDistance(4, 16, frame)
 
-                    if l < 40:
-                        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0),
+
+                    if l < 50:
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0),
                                       cv2.FILLED)
                         cv2.putText(frame, button.text, (x + 19, y + 57), cv2.FONT_HERSHEY_PLAIN, 4,
                                     (255, 255, 255), 5)
@@ -118,13 +126,22 @@ def start():
                         buttons = createKeybord((20, 20), flag)
                         sleep(0.25)
 
+                    if delete_l < 30:
+                        keybordCon.press(keyboard.Key.backspace)
+                        text = text[:-1]
+                        sleep(0.25)
+                    if enter_l < 25:
+                        keybordCon.press(keyboard.Key.enter)
+                        text = ""
+
+
 
 
 
              #Вывод текста
-        cv2.rectangle(frame, (50, 400), (50 + 400, 400 + 100), (0, 0, 255),
-                      cv2.FILLED)  # filled заливает
-        cv2.putText(frame, text, (50 + 19, 400 + 57), cv2.FONT_HERSHEY_PLAIN, 4,
+        # cv2.rectangle(frame, (50, 600), (50 + 400, 600 + 100), (0, 0, 255),
+        #               cv2.FILLED)  # filled заливает
+        cv2.putText(frame, text, (50 + 19, 600 + 57), cv2.FONT_HERSHEY_PLAIN, 4,
                     (255, 255, 255), 5)
 
 
